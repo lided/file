d9fb2936efb2b73ba8e3efaf9903114c466aba7c
lided <2747217918@qq.com>
2025-03-25 19:11:00 +0800

    add rename_file_md5

---
+++ rename_file_md5.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# 修改文件名(不包括后缀)为MD5值
+# eg rename.sh [filename]
+
+if [ "$#" -ne 1 ]; then
+	echo "must give a filename or dirname"
+	exit 1
+fi
+
+function rename_file_to_md5(){
+	file=$1
+	# 获取文件扩展名
+	extension="${file##*.}"
+	# 计算文件的MD5哈希值
+	md5sum=$(md5sum "$file" | awk '{print $1}')
+	# 获取文件所在的目录
+	dirpath="$(dirname "$file")"
+	# 重命名文件
+	mv "$file" "$dirpath/$md5sum.$extension" && echo "File $1 renamed to: $md5sum.$extension"
+}
+
+if [ -f $1  ]; then
+	rename_file_to_md5 $1
+	exit 1
+elif [ -d $1 ]; then
+	for i in `find $1  ! -path '*/.*' -type f`
+	do
+		rename_file_to_md5 $i
+	done
+fi
